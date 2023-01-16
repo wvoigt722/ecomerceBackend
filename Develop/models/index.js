52d5b6cf684ed17621034bddb1c68cb23b130195
William Voigt <wvoigt@Williams-MacBook-Air.local>
2023-01-15 19:18:38 -0800

    added relationships to models index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,37 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tag_products",
+});
 
 module.exports = {
   Product,
